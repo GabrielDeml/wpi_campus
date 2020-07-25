d9cd0eb5dbf920a5d9f43e45557dd9b56d047ce5
TianYuFan1 <tianyufan01@gmail.com>
2020-07-25 16:02:57 -0400

    added appBar and WPI logo

---
+++ lib/home.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset("resources/appBar/wpiLogo.png", fit: BoxFit.contain, height: 32),
+          ],
+        ),
+      ),
+    );
+  }
+}
