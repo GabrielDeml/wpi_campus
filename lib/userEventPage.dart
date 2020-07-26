5415218b02a74a13c1504ce7c76748c120ab0f4d
Gabe Deml <gddeml@wpi.edu>
2020-07-26 11:07:56 -0400

    Added more stuff

--- lib/userEventPage.dart
+++ lib/userEventPage.dart
@@ -2,6 +2,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:wpi_campus/homeEvent.dart';
 
+import 'firebaseData.dart';
+
 class UserEventPage extends StatefulWidget {
   final String documentId;
 
@@ -15,7 +17,7 @@ class _UserEventPageState extends State<UserEventPage> {
   final String documentId;
 
 //  String description = '';
-  SnapShotData snapShotData = new SnapShotData();
+  FirebaseData snapShotData = new FirebaseData();
 
   _UserEventPageState(this.documentId);
 
@@ -46,23 +48,25 @@ class _UserEventPageState extends State<UserEventPage> {
 //      print(documentSnapshot.data['description']);
       setState(() {
         snapShotData.description = documentSnapshot.data['description'];
+        snapShotData.name = documentSnapshot.data['name'];
+        snapShotData.organizer = documentSnapshot.data['organizer'];
       });
     });
     return ListView(
       children: <Widget>[
 //        Text(snapShotData.description)
-//          _buildImage(context, snapShotData),
+        _buildImage(context, snapShotData),
 ////        _buildTitleText(homeEvent),
 ////        _buildChips(homeEvent),
-//          _buildGeneralInfo(documentSnapshot),
-//          _buildDescriptionHeader(),
-          _buildDescription(snapShotData),
+        _buildGeneralInfo(snapShotData),
+        _buildDescriptionHeader(),
+        _buildDescription(snapShotData),
       ],
     );
 //    });
   }
 
-  Widget _buildDescription(SnapShotData snapShotData) {
+  Widget _buildDescription(FirebaseData snapShotData) {
     final _font = TextStyle(fontSize: 18.0, color: Colors.black);
     return Padding(
       padding:
@@ -79,21 +83,29 @@ class _UserEventPageState extends State<UserEventPage> {
     );
   }
 
-  Widget _buildGeneralInfo(SnapShotData snapShotData) {
+  Widget _buildGeneralInfo(FirebaseData snapShotData) {
     final _font = TextStyle(fontSize: 16.0, color: Colors.black);
     return Padding(
       padding: EdgeInsets.only(left: 50.0, top: 15.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          Text("Organization: " + "organizer", style: _font),
-          Text("Contact: " + "contact", style: _font),
+          Text(
+            snapShotData.name,
+            style: TextStyle(
+                fontSize: 28.0,
+                color: Colors.black,
+                fontWeight: FontWeight.bold),
+          ),
+          Text("Organizer: " + snapShotData.organizer, style: _font),
+          Text("Contact: " + snapShotData.contact, style: _font),
           Text(" "),
-          Text("Date: " + "date" + " | " + "time", style: _font),
-          Text("Location: " + "location", style: _font),
+          Text("Date: " + snapShotData.date + " | " + snapShotData.time,
+              style: _font),
+          Text("Location: " + snapShotData.location, style: _font),
           Text(" "),
-          Text("Zoom Link: " + "zoom", style: _font),
-          Text("Capacity: " + "capacity", style: _font),
+          Text("Zoom Link: " + snapShotData.zoom, style: _font),
+          Text("Capacity: " + snapShotData.capacity, style: _font),
         ],
       ),
     );
@@ -146,23 +158,16 @@ class _UserEventPageState extends State<UserEventPage> {
 //    );
 //  }
 
-  Widget _buildImage(context, SnapShotData snapShotData) {
+  Widget _buildImage(context, FirebaseData snapShotData) {
     return Container(
       width: MediaQuery.of(context).size.width,
       height: 200,
       decoration: BoxDecoration(
-//        image: DecorationImage(
-//          image: AssetImage(homeEvent.img),
-//          fit: BoxFit.cover,
-//        ),
-
-          ),
+        image: DecorationImage(
+          image: NetworkImage(snapShotData.image),
+          fit: BoxFit.cover,
+        ),
+      ),
     );
   }
-}
-
-class SnapShotData {
-  String description = '';
-  String organizer = '';
-
 }
