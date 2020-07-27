37d6de308acab92e78e83d34d854966d348ad17c
Gabe Deml <gddeml@wpi.edu>
2020-07-26 20:44:14 -0400

    Made it so android would build again

--- lib/userEvents.dart
+++ lib/userEvents.dart
@@ -1,5 +1,4 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase/firebase.dart' as WebFirebase;
 import 'package:flutter/material.dart';
 import 'package:wpi_campus/record.dart';
 import 'package:wpi_campus/userEventPage.dart';
